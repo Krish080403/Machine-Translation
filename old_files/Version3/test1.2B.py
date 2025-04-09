3e495ecafd6a6b376b1bbdb247a49f1753d226e3
krishm_08 <f20211732@hyderabad.bits-pilani.ac.in>
2025-04-09 11:44:39 +0000

    Old Files

---
+++ old_files/Version3/test1.2B.py
@@ -0,0 +1,26 @@
+import os
+
+try:
+    # Command to generate translations
+    generate_command = 'python3.10 /home/krish/content/old_files/Version3/modified_generate.py /home/krish/content/old_files/Version3/wmt22_spm/wmt22_bin --scoring chrf\
+        --max-tokens 2000 \
+        --batch-size 1 \
+        --path /home/krish/content/old_files/Version3/checkpoint1.2B/checkpoint_best.pt \
+        --fixed-dictionary /home/krish/content/old_files/Version3/wmt22_spm/model_dict.128k.txt \
+        -s hi -t mr \
+        --remove-bpe sentencepiece \
+        --beam 5 \
+        --task translation_multi_simple_epoch \
+        --lang-pairs hi-mr \
+        --decoder-langtok \
+        --encoder-langtok src \
+        --gen-subset test \
+        --dataset-impl mmap \
+        --distributed-world-size 1 \
+        --distributed-no-spawn'
+
+    # Execute command
+    os.system(generate_command)
+
+except Exception as e:
+    print(f"Error: {e}")
